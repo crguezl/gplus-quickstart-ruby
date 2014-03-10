f61a67f13bed47577b2cf4c9fbcd74caa743c3b9
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2014-03-10 21:43:35 +0000

    working

--- signin.rb
+++ signin.rb
@@ -1,30 +1,3 @@
-# Copyright 2013 Google Inc. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-
-##
-# The Google+ Ruby Quickstart lets you get started with the Google+ platform
-# in a few minutes.
-#
-# The app demonstrates:
-#  * Using the Google+ Sign-In button to get an OAuth 2.0 refresh token.
-#  * Exchanging the refresh token for an access token.
-#  * Making Google+ API requests with the access token, including getting a
-#    list of people that the user has circled.
-#  * Disconnecting the app from the user's Google account and revoking tokens.
-#
-# Author: class@google.com (Gus Class)
 require 'base64'
 require 'rubygems'
 require 'json'
@@ -53,7 +26,7 @@ $authorization = Signet::OAuth2::Client.new(
     :client_secret => $credentials.client_secret,
     :redirect_uri => $credentials.redirect_uris.first,
     :scope => PLUS_LOGIN_SCOPE)
-$client = Google::APIClient.new
+$client = Google::APIClient.new({:application_name => "sinatra-ruby-gplus"}); #,:application_version => "1.0"})
 
 
 ##
